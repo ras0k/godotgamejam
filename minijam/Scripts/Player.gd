90b26374457e9a0fd44f77ddae451d61898fb73b
Andi <43707028+andreahegedus@users.noreply.github.com>
2022-06-24 13:39:14 +0200

    Update Player.gd
    Some modification

--- minijam/Scripts/Player.gd
+++ minijam/Scripts/Player.gd
@@ -1,52 +1,25 @@
 extends KinematicBody2D
 
+var speed : int = 400
+var jump_speed : int = -200
+var gravity : int = 200
+var velocity = Vector2()
 
+func get_input(delta):
 
-var velocity : Vector2 = Vector2()
-
-var direction : Vector2 = Vector2()
-
-
-
-func read_input():
-
-	velocity = Vector2()
-
-	
-
-	if Input.is_action_pressed("up"):
-
-		velocity.y -= 1
-
-		direction = Vector2(0, -1)
-
-	if Input.is_action_pressed("down"):
-
-		velocity.y += 1
-
-		direction = Vector2(0, 1)
+	velocity.x = 0
 
 	if Input.is_action_pressed("left"):
-
-		velocity.x -= 1
-
-		direction = Vector2(-1, 0)
-
+		velocity.x -= speed
 	if Input.is_action_pressed("right"):
-
-		velocity.x += 1
-
-		direction = Vector2 (1, 0)
-
+		velocity.x += speed
+	if Input.is_action_pressed("jump"):
+		if (is_on_floor()):
+			velocity.y -= jump_speed
+			
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	pass
 	
-
-	velocity = velocity.normalized()
-
-	velocity = move_and_slide(velocity * 200)
-
-	
-
 func _physics_process(delta):
-
-		read_input()
-
+	get_input(delta)
