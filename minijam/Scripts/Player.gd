160c2a6f58f247b445daae55fd67f7a32b7e3138
Leonid Skvortsov <leonidXo@gmail.com>
2022-06-25 01:54:53 +0300

    Tweak movement a bit (#6)
    * Fix simultaneous up and attack causing a lock
    
    * Tweak movement
    
    * Use Scripts/Player.gd

--- minijam/Scripts/Player.gd
+++ minijam/Scripts/Player.gd
@@ -12,44 +12,45 @@ var attacking = false
 
 func _physics_process(delta):
 
-	if Input.is_action_pressed("right") && !attacking:
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play()
-		velocity.x = speed
-	elif Input.is_action_pressed("left") && !attacking:
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("run")
-		velocity.x = -speed
+	var up_pressed = Input.is_action_pressed("up")
+	var right_pressed = Input.is_action_pressed("right")
+	var left_pressed = Input.is_action_pressed("left")
+	if !attacking && (up_pressed || right_pressed || left_pressed):
+		if right_pressed || left_pressed:
+			# This formula basically assures that if both right and left are pressed
+			# at the same time the sprite direction will remain the same as it was
+			# before the press
+			$AnimatedSprite.flip_h = \
+				!left_pressed && (right_pressed || $AnimatedSprite.flip_h) || \
+				left_pressed && right_pressed && $AnimatedSprite.flip_h
+			velocity.x = speed * int(right_pressed) - speed * int(left_pressed)
+			$AnimatedSprite.play("idle" if right_pressed && left_pressed else "run")
+		if on_ground && up_pressed:
+			velocity.y = jump_power
+			on_ground = false
 	elif Input.is_action_just_pressed("attack"):
 		attacking = true;
 		print_debug("space was pressed")
 	elif attacking == true:
-		$AnimatedSprite.play("ATTACK")
+		$AnimatedSprite.play("attack")
 	else:
 		velocity.x = 0
-		if on_ground == true:
-			if attacking == false:
-				$AnimatedSprite.play("idle")
-		
-	if Input.is_action_pressed("up"):
-		if on_ground == true:
-			velocity.y = jump_power
-			on_ground = false
-			
+		if on_ground && !attacking:
+			$AnimatedSprite.play("idle")
 
 	if Input.is_action_pressed("reset"):
 		get_tree().reload_current_scene()
-	
+
 	velocity.y += gravity
 	if is_on_floor():
 		on_ground = true
 	else:
 		on_ground = false
 		if velocity.y < 0:
-				$AnimatedSprite.play("jump")
+			$AnimatedSprite.play("jump")
 		else:
-				$AnimatedSprite.play("land")
-					
+			$AnimatedSprite.play("land")
+
 	velocity = move_and_slide(velocity, FLOOR)
  
 func _on_AnimatedSprite_animation_finished():
