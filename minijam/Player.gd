791f0a86796bda840bc29cea5cac8253fcd7ab38
Andi <43707028+andreahegedus@users.noreply.github.com>
2022-06-24 23:18:48 +0200

    My whole folder

--- minijam/Player.gd
+++ minijam/Player.gd
@@ -1,51 +1,57 @@
 extends KinematicBody2D
 
+const speed = 60
+const jump_power = -115
+const gravity = 5
+const FLOOR = Vector2(0, -1)
+var velocity = Vector2()
 
+var on_ground = false
+var attacking = false
 
-var velocity : Vector2 = Vector2()
-
-var direction : Vector2 = Vector2()
-
-
-
-func read_input():
-
-	velocity = Vector2()
-
-	
-
-	if Input.is_action_pressed("up"):
-
-		velocity.y -= 1
-
-		direction = Vector2(0, -1)
-
-	if Input.is_action_pressed("down"):
-
-		velocity.y += 1
-
-		direction = Vector2(0, 1)
-
-	if Input.is_action_pressed("left"):
-
-		velocity.x -= 1
-
-		direction = Vector2(-1, 0)
-
-	if Input.is_action_pressed("right"):
-
-		velocity.x += 1
-
-		direction = Vector2 (1, 0)
-
-	
-
-	velocity = velocity.normalized()
-
-	velocity = move_and_slide(velocity * 200)
-
-	
 
 func _physics_process(delta):
 
-		read_input()
+	if Input.is_action_pressed("right") && !attacking:
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("run")
+		velocity.x = speed
+	elif Input.is_action_pressed("left") && !attacking:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("run")
+		velocity.x = -speed
+	elif Input.is_action_just_pressed("attack"):
+		attacking = true;
+		print_debug("space was pressed")
+	elif attacking == true:
+		$AnimatedSprite.play("attack")
+	else:
+		velocity.x = 0
+		if on_ground == true:
+			if attacking == false:
+				$AnimatedSprite.play("idle")
+		
+	if Input.is_action_pressed("up"):
+		if on_ground == true:
+			velocity.y = jump_power
+			on_ground = false
+			
+
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
+	
+	velocity.y += gravity
+	if is_on_floor():
+		on_ground = true
+	else:
+		on_ground = false
+		if velocity.y < 0:
+				$AnimatedSprite.play("jump")
+		else:
+				$AnimatedSprite.play("land")
+					
+	velocity = move_and_slide(velocity, FLOOR)
+ 
+func _on_AnimatedSprite_animation_finished():
+	attacking=false
+	print_debug("end of attack")
