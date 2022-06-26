73d144966de2f6c39049ad678645118a70d18dbb
Andi <43707028+andreahegedus@users.noreply.github.com>
2022-06-26 13:28:23 +0200

    import new background

--- minijam/Scripts/Menu.gd
+++ minijam/Scripts/Menu.gd
@@ -1,8 +1,10 @@
 extends Node
 
+func _ready():
+	$VBoxContainer/Start.grab_focus()
 func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/StageOne.tscn")
 
 
 func _on_Exit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
