73ce5280e205116aa0c5bfbcac39fa20a0a53615
Bran024 <40938505+Bran024@users.noreply.github.com>
2022-07-02 13:26:43 -0400

    enemy readability
    update variable and function names to improve readability

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
 
-export var A = 200
+export var aggro_range = 200
 #maximum range enemy will chase player from
-export var S = 110
+export var speed = 110
 #speed
-var D : Vector2
+var direction : Vector2
 #direction
-var prevD = Vector2(0, 0)
+var prev_direction = Vector2(0, 0)
 #previous direction
 var collisionCD = 0
 #attempts to prevent enemies from trying to walk thru obstacles
@@ -20,37 +20,37 @@ func _ready():
 	
 
 func _on_Timer_timeout():
-	var pPos = player.position - position
+	var player_pos = player.position - position
 	#calculate position of player relative to enemey
-	if pPos.length() <= 16:
+	if player_pos.length() <= 16:
 		#enemy turn to face when close
-		D = Vector2.ZERO
-		prevD = pPos.normalized()
+		direction = Vector2.ZERO
+		prev_direction = player_pos.normalized()
 		$AnimationPlayer.play("Chase")
-	elif pPos.length() <= A and collisionCD == 0:
+	elif player_pos.length() <= aggro_range and collisionCD == 0:
 		# If player is within range, move toward it
-		D = pPos.normalized()
+		direction = player_pos.normalized()
 		$AnimationPlayer.play("Chase")
 	elif collisionCD == 0:
 		$AnimationPlayer.play("Idle")
 		#psuedorandomly choose movement direction when not engaged with player
 		var random_number = rng.randf()
 		if random_number < 0.05:
-			D = Vector2.ZERO
+			direction = Vector2.ZERO
 		elif random_number < 0.1:
-			D = Vector2.DOWN.rotated(rng.randf() * 2 * PI)
+			direction = Vector2.DOWN.rotated(rng.randf() * 2 * PI)
 	
 	# Update collision cooldown
 	if collisionCD > 0:
 		collisionCD = collisionCD - 1
 
 func _physics_process(delta):
-	var M = D * S * delta
+	var movement = direction * speed * delta
 	#movement
 	
-	var C = move_and_collide(M)
+	var collision = move_and_collide(movement)
 	#collision
 	
-	if C != null and C.collider.name != "Player":
-		D = D.rotated(rng.randf_range(PI/4, PI/2))
+	if collision != null and collision.collider.name != "Player":
+		direction = direction.rotated(rng.randf_range(PI/4, PI/2))
 		collisionCD = rng.randi_range(2, 5)
