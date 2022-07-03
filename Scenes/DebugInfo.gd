c42532f425e7666e489590ba5475aab6bc46bd58
Qubus0 <steen.rickmer@gmx.de>
2022-07-03 03:03:48 +0200

    add debugging helper

---
+++ Scenes/DebugInfo.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+class_name DebugInfo
+
+
+func _ready() -> void:
+	if OS.is_debug_build():
+		if not ProjectSettings.get_setting("global/show_debug"):
+			visible = false
+			print('Info: Project setting "global/show_debug" is disabled.')
+
+
+func log_text(name: String, text: String):
+	var label: Label = $Panel/VBoxContainer.get_node_or_null(name)
+	if not label:
+		label = Label.new()
+		label.name = name
+		$Panel/VBoxContainer.add_child(label)
+	label.text = name + ": " + text
+
+
+# only works when debug collision shapes visible
+func log_radius(name: String, radius):
+	if not (typeof(radius) == TYPE_INT or typeof(radius) == TYPE_REAL):
+		return
+	var shape: CollisionShape2D = get_node_or_null(name)
+	if not shape:
+		shape = CollisionShape2D.new()
+		shape.name = name
+		add_child(shape)
+		shape.shape = CircleShape2D.new()
+	shape.shape.radius = radius
