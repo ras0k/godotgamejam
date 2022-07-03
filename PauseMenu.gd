7665935dac8856e347561e4cb0b495005dbbb695
Bran024 <40938505+Bran024@users.noreply.github.com>
2022-07-03 14:59:19 -0400

    Pause Menu
    Functioning pause menu added

---
+++ PauseMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		self.is_paused = !is_paused
+
+func _on_Resume_pressed():
+	self.is_paused = false
+
+
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
