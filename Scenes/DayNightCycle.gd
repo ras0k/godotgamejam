88c49fe11cabb803c7b979afd9ea06be4397c4b9
ADDCTION <93648571+ADDlCT@users.noreply.github.com>
2022-07-06 20:32:03 +0200

    Change of day and night

---
+++ Scenes/DayNightCycle.gd
@@ -0,0 +1,11 @@
+extends CanvasModulate
+
+const _Night_color = Color("122d30")
+const _Day_color = Color("ffffff")
+const _Time_scale = 0.01
+
+var time = 0
+
+func _process(delta:float) -> void:
+	self.time += delta * _Time_scale
+	self.color = _Night_color.linear_interpolate(_Day_color, abs(sin(time)))
