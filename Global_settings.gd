bcede51fcb1bc3cbe37520b2ee3cf530879cd5c8
ADDCTION <93648571+ADDlCT@users.noreply.github.com>
2022-07-08 01:53:43 +0200

    Redesigned settings menu

--- Global_settings.gd
+++ Global_settings.gd
@@ -1,4 +1,24 @@
 extends Node
 
+signal fps_displayed(value)
+signal fov_apdate(value)
+
 func toggle_fullscreen(value):
 	OS.window_fullscreen = value
+	
+func toggle_vsync(value):
+	OS.vsync_enabled = value
+	
+func toggle_fps_display(value):
+	emit_signal("fps_displayed", value)
+
+func set_max_fps(value):
+	Engine.target_fps = value if value < 240 else 0 
+	
+	
+	
+func update_master_vol(vol):
+	AudioServer.set_bus_volume_db(0, vol)
+	
+func update_fov(value):
+	emit_signal("fov_apdate", value)
