b115943ec599529add8878c64665d8e4d3402ee1
Bran024 <40938505+Bran024@users.noreply.github.com>
2022-08-04 22:46:51 -0400

    UI fix

--- UI.gd
+++ UI.gd
@@ -1,19 +1,18 @@
-extends CanvasLayer
-
+extends Node2D
 
 onready var player = get_node("/root/Main/Spaceship")
-onready var c = $CompassSprite
+onready var c = $CanvasLayer/CompassSprite
 
 
 func _ready():
-	$FlameSprite.playing = true
-	$FlameSprite2.playing = true
-	$FlameSprite3.playing = true
-	$FlameSprite4.playing = true
-	$FlameSprite5.playing = true
-	$FlameSprite6.playing = true
-	$FlameSprite7.playing = true
-	$FlameSprite8.playing = true
+	$CanvasLayer/FlameSprite.playing = true
+	$CanvasLayer/FlameSprite2.playing = true
+	$CanvasLayer/FlameSprite3.playing = true
+	$CanvasLayer/FlameSprite4.playing = true
+	$CanvasLayer/FlameSprite5.playing = true
+	$CanvasLayer/FlameSprite6.playing = true
+	$CanvasLayer/FlameSprite7.playing = true
+	$CanvasLayer/FlameSprite8.playing = true
 
 
 func _process(_delta):
@@ -22,92 +21,92 @@ func _process(_delta):
 
 
 func rotate_compass(frame: int) -> void:
-	$CompassSprite.frame = frame
+	c.frame = frame
 
 
 func flames_render():
 	if player.flames_on == false:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 		return
 	if c.frame == 1 or c.frame == 0 or c.frame == 15:
-		$FlameSprite.visible = true
-		$FlameSprite2.visible = true
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = true
+		$CanvasLayer/FlameSprite2.visible = true
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 2:
-		$FlameSprite.visible = true
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = true
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = true
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = true
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 3 or c.frame == 4 or c.frame == 5:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = true
-		$FlameSprite4.visible = true
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = true
+		$CanvasLayer/FlameSprite4.visible = true
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 6:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = true
-		$FlameSprite5.visible = true
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = true
+		$CanvasLayer/FlameSprite5.visible = true
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 7 or c.frame == 8 or c.frame == 9:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = true
-		$FlameSprite6.visible = true
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = true
+		$CanvasLayer/FlameSprite6.visible = true
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 10:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = true
-		$FlameSprite7.visible = true
-		$FlameSprite8.visible = false
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = true
+		$CanvasLayer/FlameSprite7.visible = true
+		$CanvasLayer/FlameSprite8.visible = false
 	if c.frame == 11 or c.frame == 12 or c.frame == 13:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = false
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = true
-		$FlameSprite8.visible = true
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = false
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = true
+		$CanvasLayer/FlameSprite8.visible = true
 	if c.frame == 14:
-		$FlameSprite.visible = false
-		$FlameSprite2.visible = true
-		$FlameSprite3.visible = false
-		$FlameSprite4.visible = false
-		$FlameSprite5.visible = false
-		$FlameSprite6.visible = false
-		$FlameSprite7.visible = false
-		$FlameSprite8.visible = true
+		$CanvasLayer/FlameSprite.visible = false
+		$CanvasLayer/FlameSprite2.visible = true
+		$CanvasLayer/FlameSprite3.visible = false
+		$CanvasLayer/FlameSprite4.visible = false
+		$CanvasLayer/FlameSprite5.visible = false
+		$CanvasLayer/FlameSprite6.visible = false
+		$CanvasLayer/FlameSprite7.visible = false
+		$CanvasLayer/FlameSprite8.visible = true
 
 func speed_meter():
-	$SpeedMeter.value = 25 * clamp(get_node("/root/Main/SolarSystem/JupiterBody/Jupiter").relativeSpeed.length(), 0.5, 5.5)
+	$CanvasLayer/SpeedMeter.value = 25 * clamp(get_node("/root/Main/SolarSystem/JupiterBody/Jupiter").relativeSpeed.length(), 0.5, 5.5)
