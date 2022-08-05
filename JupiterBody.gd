98defebb16111beba1d451b8f09fe1197f6faec2
zacYP2 <zachariedostaler@hotmail.com>
2022-08-04 23:50:16 -0400

    a few fixes and atuff

--- JupiterBody.gd
+++ JupiterBody.gd
@@ -9,8 +9,8 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_axis_velocity(Vector2(-5,10) * 0.5)
-	pass # Replace with function body.
-
+	$Sprite.frame = int(rand_range(0.0 , 11.99)) 
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
