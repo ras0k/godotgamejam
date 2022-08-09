386950df1bca8c43f651a026e7d6bef38cbc748a
EvPer <evariste.perreau@protonmail.com>
2022-08-09 17:57:39 +0200

    Spawn_asteroid now spawn in a belt

--- SolarSystem.gd
+++ SolarSystem.gd
@@ -4,6 +4,8 @@ onready var sun = $Sol
 var asteroid := preload("res://Asteroid.tscn")
 
 var rng := RandomNumberGenerator.new()
+export var min_belt_radius = 40 
+export var max_belt_radius = 120
 
 
 func _ready() -> void:
@@ -13,7 +15,11 @@ func _ready() -> void:
 func spawn_asteroid():
 	var new_asteroid: RigidBody2D = asteroid.instance()
 	add_child(new_asteroid)
-	new_asteroid.position = Vector2(randi_range_pos_neg(40, 120), randi_range_pos_neg(40, 120))
+	
+	var randA = rand_range(-3.3 , 3.3) #why doesn't -1,1 work ?
+	var randR = rand_range(min_belt_radius , max_belt_radius)
+	new_asteroid.position = Vector2(randR * cos(randA), randR * sin(randA)) + sun.position
+	#new_asteroid.position = Vector2(randi_range_pos_neg(40, 120), randi_range_pos_neg(40, 120))
 
 	var sun_distance = new_asteroid.global_position - sun.global_position
 	var normal = Vector2(-sun_distance.x, sun_distance.y)
